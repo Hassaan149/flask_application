9ba12f7b4894e0f7adee802df949a1dc30738d15
Hassaan <76567880+Hassaan149@users.noreply.github.com>
2021-12-27 19:27:16 +0500

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,14 @@
 
 from flask import Flask, request, render_template
 from scriptFin import reccomndation
+from pymongo import MongoClient
+from pandas import DataFrame
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import LabelEncoder
 app = Flask(__name__)
 def get_data():
     data = []
-    from pymongo import MongoClient
+    
     client = MongoClient(
             "mongodb+srv://Hassaan149:random098123@user-credentials.qgy2m.mongodb.net/UserDB?retryWrites=true&w=majority")
     db = client["UserDB"]
@@ -16,11 +20,10 @@ def get_data():
     return data
 
 def recc(Budget, Est, Type):
-    from pandas import DataFrame
-    from sklearn.ensemble import RandomForestClassifier
+    
     data = DataFrame(get_data())
     data.drop(['_id'], axis=1)
-    from sklearn.preprocessing import LabelEncoder
+   
     leApph = LabelEncoder().fit_transform(data.APPH)
     leStime = LabelEncoder().fit_transform(data.S_time)
     leGen = LabelEncoder().fit_transform(data.genre)
